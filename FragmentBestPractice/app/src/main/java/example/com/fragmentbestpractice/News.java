5d05981430b56580143b280256a4e6463f02db0a
SuperLoo <yuanjinglu1994@outlook.cm>
2018-06-18 21:35:45 +0800

    fragment

---
+++ FragmentBestPractice/app/src/main/java/example/com/fragmentbestpractice/News.java
@@ -0,0 +1,31 @@
+package example.com.fragmentbestpractice;
+
+/**
+ * Created by jingluyuan on 5/29/18.
+ */
+
+public class News {
+    private String title;
+
+    private String content;
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+}
